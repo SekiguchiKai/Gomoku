fc71cb09de6d36345b32e77d96cf529885072e84
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-01-10 17:21:37 +0900

    GomokuScoreCalculatorクラスのマジックナンバーを解消

---
+++ src/test/java/jp/co/topgate/kai/sekiguchi/ox/calculator/GomokuScoreCalculatorTest.java
@@ -0,0 +1,26 @@
+package jp.co.topgate.kai.sekiguchi.ox.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * GomokuScoreCalculatorクラスをテストするためのクラス
+ * Created by sekiguchikai on 2017/01/10.
+ */
+public class GomokuScoreCalculatorTest {
+
+    /**
+     *
+     */
+    @Test
+    public void calcScore()  {
+
+    }
+
+    @Test
+    public void calcLineScore()  {
+
+    }
+
+}
