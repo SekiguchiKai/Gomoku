27f9b0242255fdd48d9c24aa364c5279ec59d141
SekiguchiKai <sekiguchikai@SekiguchiKai-1.local>
2017-01-17 12:48:55 +0900

    TicTacToeScoreCalculatorの可読性を修正

--- src/main/java/jp/co/topgate/kai/sekiguchi/ox/calculator/TicTacToeScoreCalculator.java
+++ src/main/java/jp/co/topgate/kai/sekiguchi/ox/calculator/TicTacToeScoreCalculator.java
@@ -30,63 +30,41 @@ public class TicTacToeScoreCalculator extends ScoreCalculator {
         final int maxPoint = 30;
         final int minPoint = -30;
 
-        // 横
-        final int maxLength = 3;
-        IntStream.range(0, maxLength).forEach(index -> movesArray[index] = gameBoard[0][index]);
-//        movesArray[0] = gameBoard[0][0];
-//        movesArray[1] = gameBoard[0][1];
-//        movesArray[2] = gameBoard[0][2];
 
+        final int maxLength = 3;
+
+
+        // row
+        IntStream.range(0, maxLength).forEach(index -> movesArray[index] = gameBoard[0][index]);
         totalScore += super.calcLineScore(movesArray, maxPoint, minPoint);
 
 
         IntStream.range(0, maxLength).forEach(index -> movesArray[index] = gameBoard[1][index]);
-//        movesArray[0] = gameBoard[1][0];
-//        movesArray[1] = gameBoard[1][1];
-//        movesArray[2] = gameBoard[1][2];
-
         totalScore += super.calcLineScore(movesArray, maxPoint, minPoint);
 
 
         IntStream.range(0, maxLength).forEach(index -> movesArray[index] = gameBoard[2][index]);
-//        movesArray[0] = gameBoard[2][0];
-//        movesArray[1] = gameBoard[2][1];
-//        movesArray[2] = gameBoard[2][2];
         totalScore += super.calcLineScore(movesArray, maxPoint, minPoint);
 
-        // 縦
+        // column
         IntStream.range(0, maxLength).forEach(index -> movesArray[index] = gameBoard[index][0]);
-
-//        movesArray[0] = gameBoard[0][0];
-//        movesArray[1] = gameBoard[1][0];
-//        movesArray[2] = gameBoard[2][0];
         totalScore += super.calcLineScore(movesArray, maxPoint, minPoint);
 
 
         IntStream.range(0, maxLength).forEach(index -> movesArray[index] = gameBoard[index][1]);
-
-//        movesArray[0] = gameBoard[0][1];
-//        movesArray[1] = gameBoard[1][1];
-//        movesArray[2] = gameBoard[2][1];
         totalScore += super.calcLineScore(movesArray, maxPoint, minPoint);
 
 
         IntStream.range(0, maxLength).forEach(index -> movesArray[index] = gameBoard[index][2]);
-//        movesArray[0] = gameBoard[0][2];
-//        movesArray[1] = gameBoard[1][2];
-//        movesArray[2] = gameBoard[2][2];
         totalScore += super.calcLineScore(movesArray, maxPoint, minPoint);
 
 
-        // 斜め
+        // 左斜め
         IntStream.range(0, maxLength).forEach(index -> movesArray[index] = gameBoard[index][index]);
-
-//        movesArray[0] = gameBoard[0][0];
-//        movesArray[1] = gameBoard[1][1];
-//        movesArray[2] = gameBoard[2][2];
         totalScore += super.calcLineScore(movesArray, maxPoint, minPoint);
 
 
+        // 右斜め
         int column = 2;
 
         for (int row = 0; row < maxLength; row++) {
@@ -94,10 +72,7 @@ public class TicTacToeScoreCalculator extends ScoreCalculator {
 
             column--;
         }
-        
-//        movesArray[0] = gameBoard[0][2];
-//        movesArray[1] = gameBoard[1][1];
-//        movesArray[2] = gameBoard[2][0];
+
         totalScore += super.calcLineScore(movesArray, maxPoint, minPoint);
 
 
